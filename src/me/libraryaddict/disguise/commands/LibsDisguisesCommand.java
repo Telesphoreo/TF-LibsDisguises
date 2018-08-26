11b612bd11175811445714cdbc43236198ff8f37
Model 3 <craftyswans@gmail.com>
2018-08-25 21:31:02 -0700

    Oops

--- src/me/libraryaddict/disguise/commands/LibsDisguisesCommand.java
+++ src/me/libraryaddict/disguise/commands/LibsDisguisesCommand.java
@@ -3,12 +3,14 @@ package me.libraryaddict.disguise.commands;
 import me.libraryaddict.disguise.DisguiseConfig;
 import me.libraryaddict.disguise.utilities.LibsMsg;
 import me.libraryaddict.disguise.utilities.LibsPremium;
+import me.totalfreedom.libsdisguise.SuperAdminProvider;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
 import java.util.Iterator;
